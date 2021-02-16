999407760e5b07dbe482a3760ffdde1658a40ac1
pdang <pdang@sfu.ca>
2021-02-16 13:36:54 -0800

    finish log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,34 +1,22 @@
 class SessionsController < ApplicationController
-    ## log in using google api, omniauth
-    # def omniauth 
-    #     user = User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
-    #         u.username = auth['info']['first_name']
-    #         u.email = auth['info']['email']
-    #         u.password_digest = auth['info']['last_name']
-    #         u.password = SecureRandom.urlsafe_base64
-    #     end
-
-    #     if user.valid?
-    #         redirect_to root_path
-    #     else
-    #         flash[:message] = user.errors.full_messages.join(", ")
-    #         redirect_to root_path
-    #     end
-    # end
-
-    # private
-    # def auth
-    #     request.env['omniauth.auth']
-    # end
-
     def omniauth
         user = User.from_omniauth(request.env['omniauth.auth'])
         if user.valid?
-            # session[:user_id] = user.id
-            redirect_to root_path
+            session[:user_id] = user.id
+            redirect_to room_path, notice: "Logged in sucessfully"
         else
             flash[:message] = user.errors.full_messages.join(", ")
-            redirect_to main_path
+            flash[:notice] = "Failed to login"
+            render :new
         end
+    end
+    private
+    def auth
+        request.env['omniauth.auth']
+    end
+
+    def destroy 
+        session[:user_id] = nil
+        redirect_to root_path, notice: "Logged out"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/auth/:provider/callback', to: 'sessions#omniauth'
 
-  # get 'sessions/login'
-  # get 'sessions/logout'
-  # post 'sessions' => 'sessions#create'
+  get "logout", to: "sessions#destroy"
 
-  # get '/auth/:provider/callback' => 'sessions#create_session'
-  # post '/auth/:provider/callback' => 'sessions#create_session'
-
-  # get 'registration' => 'users#new', as: 'registration'
-
-
+  get "room", to: "room#newroom"
   root to: 'main#index'
 end
