9a5f57b5a6df11646c4949436cd1a4842272debb
pdang <pdang@sfu.ca>
2021-02-16 02:54:26 -0800

    log in w google

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,10 @@ class SessionsController < ApplicationController
     #log in using google api, omniauth
     def omniauth 
         binding.pry
+        User.find_or_create_by(uid: auth['uid'], provider: auth['provider'])
+    end
+
+    def auth
+        request.env['omniauth.auth']
     end
 end
