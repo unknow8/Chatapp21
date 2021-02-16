9fba2b48385654dd3f94d716c8773ca2f3c3b826
pdang <pdang@sfu.ca>
2021-02-16 03:42:31 -0800

    password cant be blank error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,21 @@
 class SessionsController < ApplicationController
     #log in using google api, omniauth
     def omniauth 
-        binding.pry
-        User.find_or_create_by(uid: auth['uid'], provider: auth['provider'])
+        user = User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+            u.username = auth['info']['first_name']
+            u.email = auth['info']['email']
+            u.password_digest = auth['info']['last_name']
+        end
+
+        if user.valid?
+            redirect_to root_path
+        else
+            flash[:message] = user.errors.full_messages.join(", ")
+            redirect_to root_path
+        end
     end
 
+    private
     def auth
         request.env['omniauth.auth']
     end
