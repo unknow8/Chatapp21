e7c2972e87bb5ca2f1499fc612075a27760195f4
pdang <pdang@sfu.ca>
2021-02-19 15:36:18 -0800

    allow user to change username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,21 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
     end
+
+    def edit
+    end
+
+    def update
+        if Current.user.update(user_params)
+            redirect_to Current.user
+        else
+            flash.now[:alert] = "Something went wrong. Try again"
+            render :edit
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username)
+    end
 end
