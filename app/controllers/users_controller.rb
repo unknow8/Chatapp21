ba6d1579561b4b381bb04ecdf70d81d89a6c17de
pdang <pdang@sfu.ca>
2021-02-19 16:30:33 -0800

    fix error can't display user avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     end
 
     private
-    def user_params
-        params.require(:user).permit(:username)
-    end
+        def user_params
+            params.require(:user).permit(:username, :avatar)
+        end
 end
