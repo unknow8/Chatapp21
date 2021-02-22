7fd7bdda1c62d37c0373e7382b2778c3b806c847
pdang <pdang@sfu.ca>
2021-02-22 11:52:50 -0800

    allow users to update

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,12 +5,12 @@ module UsersHelper
         end
     end
 
-    # def avatar_for(user, options = { size: 150 })
-    #     size = options[:size]
-    #     if user.avatar?
-    #         image_tag user.avatar.url(:thumb), width: size, class: "avatar-image"
-    #     else
-    #         image_tag "default-profile.png", width: size, class: "avatar-image"
-    #     end
-    # end
+    def avatar_for(user, options = { size: 150 })
+        size = options[:size]
+        if user.avatar?
+            image_tag user.avatar.url(:thumb), width: size, class: "avatar-image"
+        else
+            image_tag "default-profile.png", width: size, class: "avatar-image"
+        end
+    end
 end
