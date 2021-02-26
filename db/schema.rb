200ad351d6dab4b415fa34324354326e74f8e57b
pdang <dpthuy2000@gmail.com>
2021-02-26 12:47:40 -0800

    fix design on profile page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_133057) do
+ActiveRecord::Schema.define(version: 2021_02_20_231057) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
@@ -36,13 +36,7 @@ ActiveRecord::Schema.define(version: 2021_02_22_133057) do
     t.string "provider"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.string "avatar"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "messages", "rooms"
