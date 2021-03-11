dd8d408b5cf9053ac3f1e0040683a78cbfe2d39b
vsistant <vsistant@sfu.ca>
2021-03-12 03:07:45 +0700

    Fixed connection error

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -8,12 +8,11 @@ module ApplicationCable
 
     private
       def find_verified_user
-        if verified_user = request.env['warden'].user
+        if verified_user = User.find_by(id: cookies.encrypted[:user_id])
           verified_user
         else
           reject_unauthorized_connection
         end
       end
-      
   end
 end
