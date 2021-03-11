e4e825ecc6c73291c1b816ef52c1cf2092670efc
ala200 <leo_li_9@sfu.ca>
2021-03-11 06:53:42 -0800

    avatar fixed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,7 +8,7 @@ module UsersHelper
     def avatar_for(user, options = { size: 150 })
         size = options[:size]
         if user.avatar?
-            image_tag user.avatar.url(:thumb), width: size, class: "avatar-image"
+            image_tag user.avatar.url, width: size, class: "avatar-image"
         else
             image_tag "default-profile.png", width: size, class: "avatar-image"
         end
