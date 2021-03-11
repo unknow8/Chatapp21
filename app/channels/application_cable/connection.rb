2ad3d4622d0d380e4a58be58189cd5a7a6933003
ala200 <leo_li_9@sfu.ca>
2021-03-11 12:30:18 -0800

    connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -8,7 +8,8 @@ module ApplicationCable
 
     private
       def find_verified_user
-        if verified_user = User.find_by(id: cookies.encrypted[:user_id])
+        
+        if verified_user = request.env['warden'].user
           verified_user
         else
           reject_unauthorized_connection
