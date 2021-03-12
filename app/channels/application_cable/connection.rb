edc92243f16bb017eb9349549d8e030121edf56d
ala200 <leo_li_9@sfu.ca>
2021-03-12 10:12:04 -0800

    instantly show messages in chat

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -7,6 +7,14 @@ module ApplicationCable
     end
 
     private
-    
+    def find_verified_user
+        
+      if verified_user = request.env['warden'].user
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end
+
   end
 end
